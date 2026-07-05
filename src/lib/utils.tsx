5b9e5f0d753ff4a87cef3200ea1ed80b23cca4f5
JuhyunLee <hanaheart030@gmail.com>
2026-07-05 17:56:31 +0900

    fix: 커스텀 폰트사이즈 충돌 오류 수정

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -1,5 +1,13 @@
 import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
+import { extendTailwindMerge } from 'tailwind-merge';
+
+const twMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      'font-size': [{ text: ['96', '64', '48', '36', '32', '24', '20'] }],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
